7970fc58c777bb94fd0e21ec380ee4f872bbf7f3
Ankita Kushwaha <46566985+anku157501@users.noreply.github.com>
2020-07-06 09:24:44 +0530

    Create C_to_F.cpp
    https://practice.geeksforgeeks.org/problems/convert-celsius-to-fahrenheit/1/?track=SPCF-Mathematics&batchId=154

---
+++ C_to_F.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+
+//You need to complete this function
+double cToF(int C)
+{
+    return (double)(9*C/5+32);
+    //Your code here
+}
+
+// { Driver Code Starts.
+
+
+
+
+
+
+
+int main()
+{
+    int T;//number of testcases
+    int C,F;
+    cin >> T; //input number of testcases
+    while(T--){
+        cin >> C;//input temperature in celscius
+        cout << floor(cToF(C)) << endl; //print the output
+    }
+    return 0;
+}  // } Driver Code Ends
