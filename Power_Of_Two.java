f615f54949b8fe0e5d9e3b70544fbd80a04e79d4
Ankita Kushwaha <46566985+anku157501@users.noreply.github.com>
2020-07-13 22:12:01 +0530

    Update Power_Of_Two.java
    https://practice.geeksforgeeks.org/problems/power-of-2-1587115620/1/?track=SPCF-BitMagic&batchId=154

--- Power_Of_Two.java
+++ Power_Of_Two.java
@@ -40,3 +40,55 @@ public class Main
     	} 
 	
 }
+
+
+
+////OR
+
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+// Function to check power of two
+bool isPowerofTwo(long long n){
+        return ((n&(n-1))==0);
+    // Your code here    
+    
+}
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+        if(n<=0) // if n is less than equal to zero then it can't be a power of 2
+        {
+            cout<<"NO"<<endl; //so we print No
+            continue; //continue and check for other values
+        }
+
+         if(isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
