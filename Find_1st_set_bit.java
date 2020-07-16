a3949d51a871d24e8b862e6a4f8d17cf174b0432
Ankita Kushwaha <46566985+anku157501@users.noreply.github.com>
2020-07-16 19:43:07 +0530

    Create Find_1st_set_bit.java
    https://practice.geeksforgeeks.org/problems/find-first-set-bit-1587115620/1/?track=SPCF-BitMagic&batchId=154

---
+++ Find_1st_set_bit.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+/*  function to find position of first set 
+    bit in the given number
+ * n: given input for which we want to get
+      the position of first set bit
+ */
+unsigned int getFirstSetBit(int n){
+    int res=0;
+    while(n>0){
+        res++;
+        if(n&1==1)
+         break;
+         n=n/2;
+    }
+    if(res==0)
+    return 0;
+    return res;
+    // Your code here
+    
+    
+}
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        printf("%u\n", getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+  // } Driver Code Ends
