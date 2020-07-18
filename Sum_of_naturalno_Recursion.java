5fb9377c284e2ada7b7496eb0d79e233b780eda3
Ankita Kushwaha <46566985+anku157501@users.noreply.github.com>
2020-07-18 23:56:34 +0530

    Create Sum_of_naturalno_Recursion.java

---
+++ Sum_of_naturalno_Recursion.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=2;
+	    
+		System.out.println(sum(n));
+	}
+	public static int sum(int n){
+	    if(n==1)
+	    return 1;
+	    return n+sum(n-1);
+	}
+}
